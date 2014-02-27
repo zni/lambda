e55439a8ecd1ff841a35e30902fb3cdb22f99553
Matt Godshall <lifeinhex@gmail.com>
2014-02-26 22:20:33 -0500

    Now handling free variables... maybe.

--- src/LambdaCalculus.hs
+++ src/LambdaCalculus.hs
@@ -14,47 +14,51 @@ module LambdaCalculus (LambdaExpr(..)
 import Data.List (elemIndex)
 import Data.Maybe (maybe)
 
--- Lambda calculus expression
+-- | Lambda calculus expression
 data LambdaExpr a where
-    Var :: String -> LambdaExpr String
-    -- ^ Atom
-    L   :: LambdaExpr String  -> LambdaExpr b -> LambdaExpr b
-    -- ^ Lambda function
-    App :: LambdaExpr b -> LambdaExpr b -> LambdaExpr b
-    -- ^ Function application
+    Var :: String -> LambdaExpr String                        -- ^ Atom
+    L   :: LambdaExpr String  -> LambdaExpr b -> LambdaExpr b -- ^ Lambda function
+    App :: LambdaExpr b -> LambdaExpr b -> LambdaExpr b       -- ^ Function application
 
 instance Show (LambdaExpr a) where
     show (Var v)          = v
     show (L bound expr)   = "(λ"++show bound++". "++show expr++")"
     show (App left right) = "("++show left++" "++show right++")"
 
-{-
+
+-- | Expressions for de Bruijn indices.
 data DeBruijn a where
-    Index :: Integer -> DeBruijn Integer
-    Lf    :: DeBruijn b -> DeBruijn b
-    AppD  :: DeBruijn b -> DeBruijn b -> DeBruijn b
--}
+    Index :: Integer -> DeBruijn Integer            -- ^ Index
+    Lf    :: DeBruijn Integer -> DeBruijn Integer   -- ^ Lambda
+    AppD  :: DeBruijn b -> DeBruijn b -> DeBruijn b -- ^ Application
 
-data DeBruijn =
-    Index Integer          |
-    Lf DeBruijn            |
-    AppD DeBruijn DeBruijn
-
-instance Show (DeBruijn) where
+instance Show (DeBruijn a) where
     show (Index i)         = show i
     show (Lf exp)          = "(λ "++show exp++")"
     show (AppD left right) = "("++show left++" "++show right++")"
 
 
-toDeBruijn :: LambdaExpr a -> DeBruijn
-toDeBruijn exp = toDeBruijn' exp []
+-- | Convert lambda expression to de Bruijn indices.
+toDeBruijn     :: LambdaExpr a -> DeBruijn Integer
+toDeBruijn exp = toDeBruijn' exp [] 50
 
-toDeBruijn' (Var s) env   = Index $ getScope s env
-toDeBruijn' (L (Var s) exp) env = Lf $ toDeBruijn' exp (s:env)
-toDeBruijn' (App left right) env = AppD (toDeBruijn' left env) (toDeBruijn' right env)
+toDeBruijn'                      :: LambdaExpr a -> [String] -> Integer -> DeBruijn Integer
+toDeBruijn' (Var s)          env free =
+    let loc = getScope s env
+    in if loc == 0
+       then Index free
+       else Index loc
+toDeBruijn' (L (Var s) exp)  env free = Lf $ toDeBruijn' exp (s:env) (succ free)
+toDeBruijn' (App left right) env free = AppD (toDeBruijn' left env (succ free)) (toDeBruijn' right env (free + 2))
 
+getScope      :: String -> [String] -> Integer
 getScope s ls = toInteger $ maybe 0 succ $ elemIndex s ls
 
+-- | SKI combinators for conversion.
 sComb = L (Var "x") (L (Var "y") (L (Var "z") (App (Var "x") (App (Var "z") (App (Var "y") (Var "z"))))))
 kComb = L (Var "x") (L (Var "y") (Var "x"))
 iComb = L (Var "x") (Var "x")
+
+-- | Arbitrary test terms.
+l2 = (L (Var "x") (L (Var "y") (L (Var "z") (L (Var "n") (App (Var "x") (App (Var "z") (Var "n")))))))
+l3 = (App  l2 (Var "d"))
